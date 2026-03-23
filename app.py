b1aee35ad9b23ca37fd4cc92875f5a4d0c191b2b
IULU WAYNER MOZART GALVAO <150444419+iulu4@users.noreply.github.com>
2026-03-23 17:50:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import re
 
-def validar_codigo_sistema(codigo: str) -> bool:
+def validar_codigo_sistema(codigo: 
+str) -> bool:
     padrao = r"^GOTHAM-\d{4}$"
-    if re.match(padrao, str(codigo)):
+    if re.match(padrao, 
+str(codigo)):
         return True
     return False
 
-
 if __name__ == "__main__":
-    print(f"Validando: {validar_codigo_sistema('GOTHAM-1234')}")
+    print(f"Validando: 
+{validar_codigo_sistema('GOTHAM-1234')}")
