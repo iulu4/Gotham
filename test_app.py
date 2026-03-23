fab8fd1b32a96f6c3a2c77943ca0e678e2b63f0f
IULU WAYNER MOZART GALVAO <150444419+iulu4@users.noreply.github.com>
2026-03-23 17:48:39 -0300

    Refactor test cases for validar_codigo_sistema

--- test_app.py
+++ test_app.py
@@ -1,17 +1,10 @@
-
-from app import validar_codigo_sistema
+from app import 
+validar_codigo_sistema
 
 def test_codigo_valido():
-    # Casos que devem passar
-    assert validar_codigo_sistema("GOTHAM-0001") is True
-    assert validar_codigo_sistema("GOTHAM-9999") is True
+    assert 
+validar_codigo_sistema("GOTHAM-0001") is True
 
 def test_codigo_invalido():
-    # Casos que devem falhar
-    assert validar_codigo_sistema("ARKHAM-1234") is False
-    assert validar_codigo_sistema("GOTHAM-ABC") is False
-    assert validar_codigo_sistema("12345") is False
-
-def test_entrada_vazia():
-    # Caso de borda
-    assert validar_codigo_sistema("") is False
+    assert 
+validar_codigo_sistema("ARKHAM-1234") is False
